7c3ead404509223a1d2dd132e5ec6f4abca39396
awsab <awsahmad157@gmail.com>
2023-08-24 23:57:24 +0300

    update on test

--- src/test/java/com/assessment/storeAPI/DiscountContextImplTest.java
+++ src/test/java/com/assessment/storeAPI/DiscountContextImplTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
+ 
 import java.util.HashMap;
 import java.util.Map;
 
